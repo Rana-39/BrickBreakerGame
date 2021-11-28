1fa883af21f5f0301cdb4392dc1de98f4978399b
Rana-39 <94973941+Rana-39@users.noreply.github.com>
2021-11-29 02:38:31 +0300

    My patr
    Modifications my part with add some comments.

--- src/main/java/brickBreaker1/Gameplay.java
+++ src/main/java/brickBreaker1/Gameplay.java
@@ -15,135 +15,36 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
+//classes
 /**
  *
  * @author rannn
  */
 public class Gameplay extends JPanel implements KeyListener, ActionListener{
     private boolean play = false;
+    //the game can't start by itself 
     private int score = 0;
-    
+    //initial value of score
     private int totalBricks = 21;
-    
+    //number of bricks
     private Timer timer;
     private int delay =8;
-    
+    //ball speed
     private int playerX  =310;
-    
+    //stick position 
     private int ballposX = 120;
     private int ballposY = 350;
+    //ball position 
     private int ballXdir = -1;
     private int ballYdir = -2;
+    //ball direction 
     
-    //private MapGenerator map;
     
     public Gameplay(){
-        //map = new MapGenerator(3,7);
+   //contractor     
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
         timer = new Timer(delay, this);
         timer.start();
-          }
-    
-   /* public void paint(Graphics g){
-        //background
-        g.setColor(Color.BLACK);
-        g.fillRect(1, 1, 692,592);
-        //drawing map
-        map.draw((Graphics2D)g);
-        //borders
-        g.setColor(Color.yellow);
-        g.fillRect(0, 0, 3,592);
-        g.fillRect(0, 0, 692,3);
-        g.fillRect(691, 0,3,592);
-        //the paddle
-        g.setColor(Color.green);
-        g.fillRect(playerX,550,100,8);
-        //the ball
-        g.setColor(Color.yellow);
-        g.fillOval(ballposX,ballposY,20,20);
-        
-        g.dispose();
     }
-    
-      @Override
-    public void actionPerformed(ActionEvent e) {
-     timer.start();
-     if(play){
-         if(new Rectangle(ballposX, ballposY, 20,20).intersects(new Rectangle(playerX,550,100,8))){
-             ballYdir = -ballYdir;
-         }
-         ballposX+= ballXdir;
-         ballposY+= ballYdir;
-         if(ballposX < 0){
-             ballXdir = -ballXdir;
-         }
-         if(ballposY < 0){
-             ballYdir = -ballYdir;
-         }
-         if(ballposX < 670){
-             ballXdir = -ballXdir;
-         }
-     }
-     repaint();
-    }
-    
-    @Override
-    public void keyTyped(KeyEvent e) {}
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-    if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-        if(playerX >= 600){
-            playerX = 600;
-        } else {
-            moveRight();
-        }
-    }
-    if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-        if(playerX < 10){
-            playerX = 10;
-        } else {
-            moveLeft();
-        }
-    }
-    }
-
-    public void moveRight(){
-        play = true;
-        playerX+=20;
-    }
-    public void moveLeft(){
-        play = true;
-        playerX-=20;
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-*/
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-   
-}
