9c526204ad15976900061bd6856d7db763192696
Rana-39 <94973941+Rana-39@users.noreply.github.com>
2021-11-29 02:46:37 +0300

    modifications main with comments

--- src/main/java/brickBreaker1/Main.java
+++ src/main/java/brickBreaker1/Main.java
@@ -13,13 +13,20 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[]args){
         JFrame obj = new JFrame();
+        //create object for JFrame
         obj.setBounds(10,10,700,600);
+        //for the width and height for JFrame window
         obj.setTitle("Breakout Ball");
+        //title of window
         obj.setResizable(false);
+        //the window can't change it is size
         obj.setVisible(true);
+        //visible 
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Gameplay gameplay = new Gameplay();
+        //create object for class gameplay
         obj.add(gameplay);
+        //add gameplay(JPanel) into JFrame
         
     }
 }
