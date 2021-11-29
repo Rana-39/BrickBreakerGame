8aa6b366061cbe9580033ca62269d5ae00222845
ASEEL <94401314+ASEELALOMARI@users.noreply.github.com>
2021-11-29 23:05:27 +0300

    Update Gameplay.java
    Interface,
    player and ball movement.

--- src/main/java/brickBreaker1/Gameplay.java
+++ src/main/java/brickBreaker1/Gameplay.java
@@ -48,3 +48,94 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
         timer = new Timer(delay, this);
         timer.start();
     }
+    
+    @Override
+    public void paint(Graphics g) {
+        //background 
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+
+        //Borders
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        //the paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        //the ball
+        g.setColor(Color.yellow);
+        g.fillOval(ballposX, ballposY, 20, 20);
+
+        g.dispose();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        timer.start();
+        if(play){
+            //to make the ball bounce from the border
+            if(new Rectangle(ballposX, ballposY,20,20).intersects(new Rectangle(playerX, 550, 100, 8))){
+                ballYdir = -ballYdir;
+            }
+            //to move the ball
+            ballposX += ballXdir;
+            ballposY += ballYdir;
+            if(ballposX < 0){
+                ballXdir = -ballXdir;    
+            }
+            if(ballposY < 0){
+                ballYdir = -ballYdir;    
+            }
+            if(ballposX > 670){
+                ballXdir = -ballXdir;    
+            }
+        }
+        
+        
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    //move the player(the green rectangle)right& left
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX < 10) {
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
+    }
+
+    public void moveRight() {
+        play = true;
+        playerX += 20;
+    }
+
+    public void moveLeft() {
+        play = true;
+        playerX -= 20;
+    }
+
+}
+
