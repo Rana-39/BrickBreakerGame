d4558326b53add9e3cf6be70dfa4e46e15a8976d
Rawanmohd <94239917+Rawanmohd@users.noreply.github.com>
2021-11-30 02:06:35 +0300

    Update Gameplay.java

--- src/main/java/brickBreaker1/Gameplay.java
+++ src/main/java/brickBreaker1/Gameplay.java
@@ -3,139 +3,93 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package brickBreaker1;
-
-import java.awt.Color;
-import java.awt.Graphics;
+package bbg;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-//classes
-/**
- *
- * @author rannn
- */
-public class Gameplay extends JPanel implements KeyListener, ActionListener{
-    private boolean play = false;
-    //the game can't start by itself 
-    private int score = 0;
-    //initial value of score
-    private int totalBricks = 21;
-    //number of bricks
-    private Timer timer;
-    private int delay =8;
-    //ball speed
-    private int playerX  =310;
-    //stick position 
-    private int ballposX = 120;
-    private int ballposY = 350;
-    //ball position 
-    private int ballXdir = -1;
-    private int ballYdir = -2;
-    //ball direction 
-    
-    
-    public Gameplay(){
-   //contractor     
-        addKeyListener(this);
-        setFocusable(true);
-        setFocusTraversalKeysEnabled(false);
-        timer = new Timer(delay, this);
-        timer.start();
-    }
-    
-    @Override
-    public void paint(Graphics g) {
-        //background 
-        g.setColor(Color.black);
-        g.fillRect(1, 1, 692, 592);
 
-        //Borders
-        g.setColor(Color.yellow);
-        g.fillRect(0, 0, 3, 592);
-        g.fillRect(0, 0, 692, 3);
-        g.fillRect(691, 0, 3, 592);
+public class GamePlay extends JPanel implements KeyListener, 
+ActionListener {
+ 
+ private boolean play = false;
+ private int score = 0;
+ private int totalbricks = 21;
+ private Timer Timer;
+ private int delay = 8;
+ private int playerX = 310;
+ private int ballposX = 120;
+ private int ballposY = 350;
+ private int ballXdir = -1;
+ private int ballYdir = -2;
+ private MapGenerator map;
+public GamePlay() {
+ map = new MapGenerator(3, 7);
+ addKeyListener(this);
+ setFocusable(true);
+ setFocusTraversalKeysEnabled(false);
+ Timer = new Timer(delay, this);
+ Timer.start();
+ }
+ 
+ public void paint(Graphics g) {
+ g.setColor(Color.black);
+ g.fillRect(1, 1, 692, 592);
+ map.draw((Graphics2D) g);
+ g.setColor(Color.yellow);
+ g.fillRect(0, 0, 3, 592);
+ g.fillRect(0, 0, 692, 3);
+ g.fillRect(691, 0, 3, 592);
+ g.setColor(Color.white);
+ g.setFont(new Font("serif", Font.BOLD, 25));
+ g.drawString("" + score, 590, 30);
+ g.setColor(Color.yellow);
+ g.fillRect(playerX, 550, 100, 8);
 
-        //the paddle
-        g.setColor(Color.green);
-        g.fillRect(playerX, 550, 100, 8);
+ g.setColor(Color.GREEN);
+ g.fillOval(ballposX, ballposY, 20, 20);
+ if (ballposY > 570) {
+ play = false;
+ ballXdir = 0;
+ ballYdir = 0;
+ g.setColor(Color.red);
+ g.setFont(new Font("serif", Font.BOLD, 30));
+ g.drawString(" Game Over Score: " + score, 190, 300);
+ g.setFont(new Font("serif", Font.BOLD, 30));
+ g.drawString(" Press Enter to Restart", 190, 340);
+ }
+ if(totalbricks == 0){
+ play = false;
+ ballYdir = -2;
+ ballXdir = -1;
+ g.setColor(Color.red);
+ g.setFont(new Font("serif",Font.BOLD,30));
+ g.drawString(" Game Over: "+score,190,300);
+ g.setFont(new Font("serif", Font.BOLD, 30));
+ g.drawString(" Press Enter to Restart", 190, 340);
+ }
+ g.dispose();
+ }
 
-        //the ball
-        g.setColor(Color.yellow);
-        g.fillOval(ballposX, ballposY, 20, 20);
-
-        g.dispose();
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        timer.start();
-        if(play){
-            //to make the ball bounce from the border
-            if(new Rectangle(ballposX, ballposY,20,20).intersects(new Rectangle(playerX, 550, 100, 8))){
-                ballYdir = -ballYdir;
-            }
-            //to move the ball
-            ballposX += ballXdir;
-            ballposY += ballYdir;
-            if(ballposX < 0){
-                ballXdir = -ballXdir;    
-            }
-            if(ballposY < 0){
-                ballYdir = -ballYdir;    
-            }
-            if(ballposX > 670){
-                ballXdir = -ballXdir;    
-            }
-        }
-        
-        
-        repaint();
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-    }
-
-    //move the player(the green rectangle)right& left
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if (playerX >= 600) {
-                playerX = 600;
-            } else {
-                moveRight();
-            }
-
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            if (playerX < 10) {
-                playerX = 10;
-            } else {
-                moveLeft();
-            }
-        }
-    }
-
-    public void moveRight() {
-        play = true;
-        playerX += 20;
-    }
-
-    public void moveLeft() {
-        play = true;
-        playerX -= 20;
-    }
-
-}
-
+ public void actionPerformed(ActionEvent e) {
+ Timer.start();
+ if (play) {
+ if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new 
+Rectangle(playerX, 550, 100, 8))) {
+ ballYdir = -ballYdir;
+ }
+ A:
+ for (int i = 0; i < map.map.length; i++) {
+ for (int j = 0; j < map.map[0].length; j++) {
+ if (map.map[i][j] > 0) {
+ int brickX = j * map.bricksWidth + 80;
+int brickY = i * map.bricksHeight + 50;
+int bricksWidth = map.bricksWidth;
+int bricksHeight = map.bricksHeight;
