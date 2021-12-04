b09ea4d70a6b5b47a8c38ce281535876325f247f
Noura-G-W <80917912+Noura-G-W@users.noreply.github.com>
2021-12-04 03:22:14 +0300

    Update Main.java

--- src/main/java/brickBreaker1/Main.java
+++ src/main/java/brickBreaker1/Main.java
@@ -1,34 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author rannna
- */
 package bbg;
 import javax.swing.JFrame;
 public class BBG {
-public static void main(String[] args) {
- 
- JFrame obj = new JFrame();
- GamePlay gameplay = new GamePlay();
-    //create an object for GamePlay
- obj.setBounds(10,10,700,600);
-    //height and width of window
- obj.setTitle("BrickBreaker");
-    //the title of the window 
- obj.setResizable(false);
-    //it can't change it's size
- obj.setVisible(true);
-    //to make it visible 
- obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //exit,open and close the window 
- obj.add(gameplay);
-    //called gameplay as argument in JFrame
- }
- 
-
-}
+    public static void main(String[] args) {
+       
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }}
